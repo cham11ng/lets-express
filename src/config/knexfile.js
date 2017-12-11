5e884ea3c1169541e27d0c5c2fa2b25b7e3b142f
Sagar Chamling <sgr.raee@gmail.com>
2017-12-12 00:29:44 +0545

    Reconfigure travis for postgres

--- src/config/knexfile.js
+++ src/config/knexfile.js
@@ -12,7 +12,7 @@ module.exports = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database:
-      process.env.NODE_ENV === 'test'
+      process.env.APP_ENV === 'test'
         ? process.env.TEST_DB_NAME
         : process.env.DB_NAME,
     charset: 'utf8',
